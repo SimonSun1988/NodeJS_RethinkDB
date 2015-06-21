48ae08c88f070052b50276a29b20e3b3ef8125f8
Simon Sun <blackghost0118@gmail.com>
2015-06-22 00:31:10 +0800

    add socket.io

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var r = require('rethinkdb');
 var bodyParser = require('body-parser');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+var EventEmitter = require('events').EventEmitter;
+var bomb = new EventEmitter();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,7 +27,8 @@ r.connect({
     r.table('posts').changes().run(conn, function (err, cursor) {
         cursor.each(function (err, row) {
             if (err) throw err;
-            console.log(row);
+            // console.log(row);
+            bomb.emit('explode', { data: row } );
         });
     });
 });
@@ -31,7 +38,8 @@ app.post('/', function ( req, res, next ) {
     var name = req.body.name;
     var age = req.body.age;
     r.table('posts').insert({
-        name: name, age: age
+        name: name,
+        age: age
     }, { returnChanges: true }).run(conn, function (err, doc) {
         console.log(err);
         return res.json(doc);
@@ -48,4 +56,15 @@ app.get('/', function ( req, res, next ) {
 
 
 
-app.listen(9291);
+server.listen(9291);
+
+bomb.on('explode', function(data) {
+    console.log('data', data.data);
+});
+
+
+io.on('connection', function (socket) {
+    socket.on('insertData', function (data) {
+        console.log('data', data);
+    });
+});
